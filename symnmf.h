a2e94d429165351a071563d44f348e578e288dac
Noam Shaib <100312556+Noamshaib@users.noreply.github.com>
2023-09-26 22:11:12 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -5,7 +5,7 @@ double **symnmf_c(double **pointsMatrix, int N, int dim, int k);
 double **sym_c(double **pointsMatrix, int N, int dim);
 double **ddg_c(double **pointsMatrix, int N, int dim);
 double **norm_c(double **pointsMatrix, double **pointsMatrix, int N, int dim);
-void freeMatrix(double **Matrix);
-
+void free_Matrix(double **head);
+void print_Matrix(double **head);
 
 # endif
