d96f4d13f2176d8f4e8710c73633a0719198e4a3
Noam Shaib <100312556+Noamshaib@users.noreply.github.com>
2023-09-28 12:17:26 +0300

    Update symnmf.py

--- symnmf.py
+++ symnmf.py
@@ -1,41 +1,56 @@
 import sys
 import pandas as pd
 import numpy as np
-import mykmeanssp as mkm
+import mysymnmfsp as msm
+np.random.seed(0)
 
 if len(sys.argv) == 3:
-    goal = argv[1]
-    inputfile = argv[2]
+    goal = sys.argv[1]
+    inputfile = srs.argv[2]
 else:
-    k = argv[1]
-    goal = argv[2]
-    inputfile = argv[3]
+    k = sys.argv[1]
+    goal = sys.argv[2]
+    inputfile = sys.argv[3]
 
 X = defX(inputfile)
+dim = len(X[0])
+N = len(X)
 
 if goal == "symnmf":
-    W = norm(X, N, dim)
-    H0 = initial_H(W)
-    H = symnmf(H0, W, N, dim)
+    W = msm.norm(X, N, dim)
+    H0 = initial_H(W, k, dim)
+    H = msm.symnmf(W, H0, k, N, dim)
     printMatrix(H)
 
 elif goal == "sym":
-    A = sym(X, N, dim)
+    A = msm.sym(X, N, dim)
     printMatrix(H)
 
 elif goal == "ddg":
-    D = ddg(X, N, dim)
+    D = msm.ddg(X, N, dim)
     printMatrix(D)
 
 else:  # goal == "norm"
-    W = norm(X, N, dim)
+    W = msm.norm(X, N, dim)
     printMatrix(W)
 
 
-defX(filename):
+def defX(filename):
     return pd.read_csv(filename, header=None)
 
-def initial_H(W):
-    H0 =
-
+def initial_H(W, k, n):
+    W_np = np.array(W)
+    m = np.mean(W_np)
+    upper_bound = 2.0 * np.sqrt(m / k)
+    H0_np = np.random.uniform(0, upper_bound, size=(n, n))
+    H0 = H0_np.tolist()
     return H0
+
+def printMatrix(M):
+    for vector in M:
+        ret_val = ""
+        for cord in vector:
+            ret_val = ret_val + str("{:.4f}".format(cord)) +","
+        ret_val = ret_val[:len(ret_val)-1]
+        print(ret_val)
+    return None
