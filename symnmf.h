deec0ba313bbe3094813a8ec0a6e8e7d107e7f09
Noam Shaib <100312556+Noamshaib@users.noreply.github.com>
2023-09-29 17:29:54 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,12 +1,15 @@
 # ifndef SYM_NMF
 # define SYM_NMF
 
-double **symnmf_c(double **H, double **W, int k, int N, int dim);
-double **sym_c(double **X, int N, int dim);
-double **ddg_c(double **X, int N, int dim);
-double **norm_c(double **X, int N, int dim);
-void free_Matrix(double **head);
-void print_Matrix(double **head); /*only to check the code, printing in this project is from s-f.py or s-f.c*/
+
+double **sym_c(double **pointsMatrix, int N, int dim); 
+double **ddg_c(double **pointsMatrix, int N, int dim);
+double **norm_c(double **pointsMatrix, int N, int dim);
+double **symnmf_c(double **initialH, double **W, int N, int k);
+
+
+void ptintMatrix(double **mtrx, int rows, int cols);        /*only to check the code, printing in this project is from s-f.py or s-f.c*/
+void freeMatrix(double **mtrx, int rows);
 
 
 # endif
