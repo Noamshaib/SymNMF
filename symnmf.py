cafa4dc999b62993ae968ed70cebb29896120ca5
Noam Shaib <100312556+Noamshaib@users.noreply.github.com>
2023-09-29 17:29:30 +0300

    Update symnmf.py

--- symnmf.py
+++ symnmf.py
@@ -4,11 +4,32 @@ import numpy as np
 import mysymnmfsp as msm
 np.random.seed(0)
 
+def defX(filename):
+    return pd.read_csv(filename, header=None).values.tolist()
+
+def initial_H(W, k, n):
+    W_np = np.array(W)
+    m = np.mean(W_np)
+    upper_bound = 2 * np.sqrt(m / k)
+    H0_np = np.random.uniform(0, upper_bound, size=(n, k))
+    H0 = H0_np.tolist()
+    return H0
+
+def printMatrix(M):
+    for vector in M:
+        ret_val = ""
+        for cord in vector:
+            ret_val = ret_val + str("{:.4f}".format(cord)) +","
+        ret_val = ret_val[:len(ret_val)-1]
+        print(ret_val)
+    return None
+
+
 if len(sys.argv) == 3:
     goal = sys.argv[1]
     inputfile = sys.argv[2]
 else:
-    k = sys.argv[1]
+    k = int(sys.argv[1])
     goal = sys.argv[2]
     inputfile = sys.argv[3]
 
@@ -24,7 +45,7 @@ if goal == "symnmf":
 
 elif goal == "sym":
     A = msm.sym(X, N, dim)
-    printMatrix(H)
+    printMatrix(A)
 
 elif goal == "ddg":
     D = msm.ddg(X, N, dim)
@@ -34,23 +55,3 @@ else:  # goal == "norm"
     W = msm.norm(X, N, dim)
     printMatrix(W)
 
-
-def defX(filename):
-    return pd.read_csv(filename, header=None).values.tolist()
-
-def initial_H(W, k, n):
-    W_np = np.array(W)
-    m = np.mean(W_np)
-    upper_bound = 2.0 * np.sqrt(m / k)
-    H0_np = np.random.uniform(0, upper_bound, size=(n, n))
-    H0 = H0_np.tolist()
-    return H0
-
-def printMatrix(M):
-    for vector in M:
-        ret_val = ""
-        for cord in vector:
-            ret_val = ret_val + str("{:.4f}".format(cord)) +","
-        ret_val = ret_val[:len(ret_val)-1]
-        print(ret_val)
-    return None
