8f81aa8fca95f31973855ac810a5087bb81e92a0
Noam Shaib <100312556+Noamshaib@users.noreply.github.com>
2023-09-30 23:12:02 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -7,6 +7,7 @@ double **ddg_c(double **pointsMatrix, int N, int dim);
 double **norm_c(double **pointsMatrix, int N, int dim);
 double **symnmf_c(double **initialH, double **W, int N, int k);
 
+double* kmeans_c(int Pyk, int Pyiter,int Pynumofrows,int Pydim ,double Pyepsilon,double** C_Matrix, double* C_Centroids);
 
 void ptintMatrix(double **mtrx, int rows, int cols);        /*only to check the code, printing in this project is from s-f.py or s-f.c*/
 void freeMatrix(double **mtrx, int rows);
